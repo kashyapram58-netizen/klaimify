dce86a1221c9e357f5a57da5bae0c27c65841a03
Your Name <your@email.com>
2026-06-27 08:20:50 +0000

    fixed library member logic

--- klaimify/klaimify_lms/doctype/library_member/library_member.py
+++ klaimify/klaimify_lms/doctype/library_member/library_member.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2026, Ram and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class LibraryMember(Document):
-	pass
+	def get_outstanding_fines(self):
+			# Calculate sum of unpaid fines for this member
+			total_unpaid = frappe.db.sql("""
+				SELECT SUM(amount - paid_amount) 
+				FROM `tabLibrary Fine` 
+				WHERE member = %s AND status != 'Paid'
+			""", self.name)[0][0]
+			
+			return total_unpaid or 0
+	
